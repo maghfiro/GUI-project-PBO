1e1bde88e816d0670938024656bc4cdc47527498
maghfiro <maghfironurekasaputri@gmail.com>
2021-02-03 09:14:48 +0700

    tugas kelompok pbo

---
+++ src/latihangui/jPasswordField.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihangui;
+
+/**
+ *
+ * @author a
+ */
+class jPasswordField {
+
+    static Object getText() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    static void requestFocus() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
---
+++ src/latihangui/jUsernameField.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihangui;
+
+/**
+ *
+ * @author a
+ */
+class jUsernameField {
+
+    static Object getText() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    static void requestFocus() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    
+}
